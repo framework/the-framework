abff3e793d7cd384e638f1c989374f0e46ace5cb
Romuald Brillout <git@brillout.com>
2026-08-21 23:30:37 +0200

    improve

--- packages/the-framework.ai/vite.config.ts
+++ packages/the-framework.ai/vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), vike()],
-  // @ts-ignore
-  // TO-DO/eventually: remove this workaround once upstream fixed the issue
-  // Temporary workaround for Vike error
-  // https://github.com/gemstack-land/the-framework/issues/460
-  vitePluginServerEntry: { disableAutoImport: true },
 })
